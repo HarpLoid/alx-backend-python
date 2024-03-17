5f51e879c7349e8fc6f0add0f40df50ff2c4c7b8
HarpLoid <walesii19@gmail.com>
2024-03-17 15:06:53 +0100

    style: fixed docs issues

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -6,4 +6,12 @@ from typing import List, Tuple, Iterable, Sequence
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """function doc
+
+    Args:
+        lst (Iterable[Sequence]): _description_
+
+    Returns:
+        List[Tuple[Sequence, int]]: _description_
+    """
     return [(i, len(i)) for i in lst]
