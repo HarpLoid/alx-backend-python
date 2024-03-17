0944dd03c4bafdd449a80adc0aa5d61413f1658d
HarpLoid <walesii19@gmail.com>
2024-03-17 15:04:41 +0100

    feat: tasks 7, 8 and 9

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,1 +1,27 @@
 #!/usr/bin/env python3
+"""
+Module 7-to_kv
+
+A type-annotated function to_kv
+that takes a string k and
+an int OR float v as arguments
+and returns a tuple.
+The first element of the tuple is the string k.
+The second element is the square of the int/float v
+and should be annotated as a float.
+"""
+from typing import Tuple, Union
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """that takes a string k and an int OR float v
+    and returns a tuple
+
+    Args:
+        k (str): string
+        v (typing.Union[int, float]): int or float
+
+    Returns:
+        typing.Tuple[str, float]: tuple of string and float
+    """
+    return (k, v ** 2)
--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -1,1 +1,35 @@
 #!/usr/bin/env python3
+"""
+Module 8-make_multiplier
+
+A type-annotated function make_multiplier
+that takes a float multiplier as argument
+and returns a function that multiplies
+a float by multiplier.
+"""
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """takes a float multiplier as argument
+    and returns a function that multiplies
+    a float by multiplier
+
+    Args:
+        multiplier (float): multiplier
+
+    Returns:
+        Callable[[float], float]: callable
+    """
+    def mult(m: float) -> float:
+        """multiplier callable
+
+        Args:
+            m (float): multiplier
+
+        Returns:
+            float: _description_
+        """
+        return m * multiplier
+
+    return mult
