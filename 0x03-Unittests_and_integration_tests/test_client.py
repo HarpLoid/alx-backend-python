4ba71cc7367640c517b2fd3ee4c7d65d2c517f33
HarpLoid <walesii19@gmail.com>
2024-04-07 16:03:58 +0100

    style: fixed pycode style

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -8,24 +8,25 @@ from unittest.mock import patch, Mock, PropertyMock
 from parameterized import parameterized, parameterized_class
 from fixtures import TEST_PAYLOAD
 
+
 class TestGithubOrgClient(unittest.TestCase):
     """
     TestGithubOrgClient class
     """
-    @parameterized.expand ([
+    @parameterized.expand([
         'google',
         'abc'
     ])
     @patch('client.get_json', return_value={'payload': True})
     def test_org(self, org_name, mock_get):
         """
-        Tests org method in client 
+        Tests org method in client
         """
         github_org_client = GithubOrgClient(org_name)
         self.assertEqual(github_org_client.org, {'payload': True})
         url = f"https://api.github.com/orgs/{org_name}"
         mock_get.assert_called_once_with(url)
-    
+
     @patch('client.GithubOrgClient.org', new_callable=PropertyMock)
     def test_public_repos_url(self, mock_org):
         """
@@ -34,8 +35,9 @@ class TestGithubOrgClient(unittest.TestCase):
         payload = {'repos_url': 'https://api.github.com/orgs/google/repos'}
         mock_org.return_value = payload
         github_org_client = GithubOrgClient("google")
-        self.assertEqual(github_org_client._public_repos_url, payload["repos_url"])
-    
+        self.assertEqual(github_org_client._public_repos_url,
+                         payload["repos_url"])
+
     @patch('client.get_json',
            return_value=[{'name': 'repo1'}, {'name': 'repo2'}])
     def test_public_repos(self, mock_get_json):
