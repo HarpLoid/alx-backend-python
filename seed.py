4be5c90e326d10a1c274152398ba49bea5754908
Harp <harploidsoft@gmail.com>
2025-09-04 05:08:28 +0100

    feat(seed): add initial database setup and data seeding
    Introduces `seed.py` to manage MySQL database setup and data population.
    Provides functions for connecting to the database server, creating the
    `ALX_prodev` database, connecting to it, creating the `user_data` table,
    and inserting data from a CSV. The `insert_data` function leverages a
    Python generator for efficient data streaming.
    
    Updates `README.md` with project objectives and instructions for the
    seeding script. Adds common Python-related files to `.gitignore`.

---
+++ seed.py
@@ -0,0 +1,99 @@
+import mysql.connector
+
+
+def connect_db():
+    """Connects to the mysql database server
+
+    Keyword arguments: none
+    Return: connection to the database server
+    """
+
+    connection = mysql.connector.connect(
+        host = "localhost",
+        user = "root",
+        password = "H07a12r94p"
+    )
+    
+    return connection
+
+def create_database(connection):
+    """Creates the database if it doesn't exist
+
+    Keyword arguments: connection
+    connection -- connection to the database server
+    Return: none
+    """
+    
+    cursor = connection.cursor()
+    cursor.execute("CREATE DATABASE IF NOT EXISTS ALX_prodev")
+    cursor.close()
+
+def connect_to_prodev():
+    """Connects to the database created on
+    the database server
+
+    Keyword arguments: none
+    Return: connection to the database
+    """
+
+    connection = mysql.connector.connect(
+        host = "localhost",
+        user = "root",
+        password = "H07a12r94p",
+        database = "ALX_prodev"
+    )
+    return connection
+
+
+def create_table(connection):
+    """Connects to the database server
+    Creates the table if it doesn't exist
+
+    Keyword arguments: connection
+    connection -- connection to the database server
+    Return: none
+    """
+    cursor = connection.cursor()
+    cursor.execute(
+        """
+        CREATE TABLE IF NOT EXISTS user_data (
+            user_id CHAR(36) NOT NULL PRIMARY KEY DEFAULT (UUID()),
+            name VARCHAR(255) NOT NULL,
+            email VARCHAR(255) NOT NULL,
+            age DECIMAL NOT NULL
+        )
+        """
+    )
+    connection.commit()
+    cursor.close()
+
+
+def insert_data(connection, data):
+    """Inserts data in the database if it does not exist
+    
+    Keyword arguments: connection, data
+    connection -- connection to the database server
+    data -- data to be inserted in the database
+    Return: none
+    """
+
+    gen = (row for row in open(data, 'r'))
+
+    # Skip the first row which is the header
+    # of the file
+    next(gen)
+
+    cursor = connection.cursor()
+    for row in gen:
+        name, email, age_str = row.replace('"', '').strip().split(',')
+        age = float(age_str)
+        values = (name, email, age)
+        cursor.execute(
+            """
+            INSERT INTO user_data (name, email, age) VALUES (%s, %s, %s)
+            ON DUPLICATE KEY UPDATE email = email;
+            """,
+            values
+        )
+    connection.commit()
+    cursor.close()
