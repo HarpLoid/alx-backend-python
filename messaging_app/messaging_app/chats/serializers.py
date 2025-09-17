a8e2f85bd6463ac3f1345f47a84f21055e48cff5
Harp <harploidsoft@gmail.com>
2025-09-17 22:50:26 +0100

    feat(api): Add messaging feature with user, message, and conversation models
    This change adds the messaging feature by creating a new app called 'chats'. It defines models for User, Message, and Conversation, and sets up API endpoints for managing them. The project settings, URLs, dependencies, and other configurations are also updated.

---
+++ messaging_app/messaging_app/chats/serializers.py
@@ -0,0 +1,27 @@
+from rest_framework import serializers
+from .models import User, Message, Conversation
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['user_id', 'first_name',
+                  'last_name', 'email',
+                  'phone_number', 'role']
+        read_only_fields = ['user_id', 'created_at']
+        
+class MessageSerializer(serializers.ModelSerializer):
+    sender = UserSerializer(source='sender_id', read_only=True)
+    class Meta:
+        model = Message
+        fields = ['message_id', 'sender', 'message_body', 'sent_at']
+        read_only_fields = ['message_id', 'sent_at']
+
+class ConversationSerializer(serializers.ModelSerializer):
+    participants = UserSerializer(many=True, read_only=True, source='participants_id')
+    messages = MessageSerializer(many=True, read_only=True)
+    class Meta:
+        model = Conversation
+        fields = ['conversation_id', 'participants', 'messages', 'created_at']
+        read_only_fields = ['conversation_id', 'created_at']
+
+
